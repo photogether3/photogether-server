c9d9b687fdb7936b1953c0560c01f7b4d6aee37d
dev.goraebap <dev.goraebap@gmail.com>
2025-02-16 13:05:32 +0900

    feat: update exception handler

--- app/exceptions/handler.ts
+++ app/exceptions/handler.ts
@@ -1,5 +1,6 @@
+import { ExceptionHandler, HttpContext } from '@adonisjs/core/http'
 import app from '@adonisjs/core/services/app'
-import { HttpContext, ExceptionHandler } from '@adonisjs/core/http'
+import { errors } from '@vinejs/vine'
 
 export default class HttpExceptionHandler extends ExceptionHandler {
   /**
@@ -13,6 +14,13 @@ export default class HttpExceptionHandler extends ExceptionHandler {
    * response to the client
    */
   async handle(error: unknown, ctx: HttpContext) {
+    if (error instanceof errors.E_VALIDATION_ERROR) {
+      return ctx.response.badRequest({
+        statusCode: 400,
+        message: error.messages[0].message,
+      })
+    }
+
     return super.handle(error, ctx)
   }
 
