e1133d9571ea6f29ec716fbc9963b3184a59cbf8
dev.goraebap <dev.goraebap@gmail.com>
2025-02-18 13:11:56 +0900

    wip: update user api

--- app/controllers/api/user_api_controller.ts
+++ app/controllers/api/user_api_controller.ts
@@ -1,9 +1,12 @@
+import { firebaseBucket, firebaseStorage } from '#config/firebase'
 import { UserDto } from '#models/dto/user.dto'
 import User from '#models/user'
 import { emailTakenValidator } from '#validators/user'
+import { Exception } from '@adonisjs/core/exceptions'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class UserApiController {
+
   async isEmailTaken({ request }: HttpContext) {
     const emailParam = request.param('email')
     const { email } = await emailTakenValidator.validate({ email: emailParam })
@@ -16,8 +19,21 @@ export default class UserApiController {
   }
 
   async updateProfile({ user, request }: HttpContext) {
-    console.log(request.all())
-    console.log(request.file('file'))
+    const file = request.file('file')
+    if (!file) {
+      throw new Exception('파일을 찾을 수 없습니다.', { status: 404, code: 'E_FILE_NOT_FOUND' })
+    }
+    request.multipart.onFile('file', {}, async (part) => {
+      const bucket = firebaseStorage.bucket(firebaseBucket)
+
+      console.log('버킷!!')
+
+      const firebaseFile = bucket.file(part.filename)
+      console.log('vkdldjqpdltm 파일!!')
+      await firebaseFile.save(part)
+      console.log('세이브 됨')
+      await firebaseFile.makePublic()
+    })
   }
 
   async updatePasswordByOtp({ request }: HttpContext) { }
