8ac9c55cdb096de6fbe9f6dae78a5bbfe3fe4f8a
dev.goraebap <dev.goraebap@gmail.com>
2025-02-21 19:47:50 +0900

    refactor

---
+++ app/controllers/web/user_web_controller.ts
@@ -0,0 +1,16 @@
+import User from "#models/user";
+import { HttpContext } from "@adonisjs/core/http";
+
+export default class UserWebController {
+
+  async index({ view }: HttpContext) {
+    const paginatedUser = await User
+      .query()
+      .preload('role')
+      .orderBy('created_at', 'desc')
+      .paginate(1, 10)
+      
+    const { meta, data } = paginatedUser.serialize()
+    return view.render('pages/users/index', { meta, data })
+  }
+}
